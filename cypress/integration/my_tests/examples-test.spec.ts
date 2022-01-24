fe86a710d459acfd6d80f6cea1aa2a709930c31c
Vyacheslav Pislyakov <66077656+slavaPislyakov@users.noreply.github.com>
2022-01-24 22:54:18 +0300

    Delete cypress/integration/my_tests directory

--- cypress/integration/my_tests/examples-test.spec.ts
+++
@@ -1,7 +0,0 @@
-describe("My first test", () => {
-  it("open google.ru", () => {
-    cy.visit("https://google.ru");
-    cy.screenshot("Capturing the screenshot after successful login");
-    cy.searchCypressInGoogle("Cypress JS", "JavaScript E111111111Testing Framework | cypress.io1111");
-  });
-});
